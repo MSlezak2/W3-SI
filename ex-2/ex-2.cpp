4abc8c4b0470d2777c3f4e352016449c91b19cef
MSlezak2 <michalsleezak@gmail.com>
2022-06-09 13:04:31 +0200

    Add storing extracted groups in vectors
    Vectors are now being used as containers for numbers and operators pending
    processing.

--- ex-2/ex-2.cpp
+++ ex-2/ex-2.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <vector>
+#include <cstdlib>
 
-void group(std::string input_string, std::regex pattern);
+template<class T>
+void group(std::string input_string, std::regex pattern, std::vector<T>& container);
 
 int main() {
 
@@ -13,29 +16,54 @@ int main() {
 	std::regex numbers_pattern("[0-9]+");
 	std::regex operators_pattern("[+-/*]");
 
-	group(users_input, numbers_pattern);
+	std::vector<int> numbers;
+	std::vector<char> operators;
 
-	group(users_input, operators_pattern);
+	group(users_input, numbers_pattern, numbers);
+
+	group(users_input, operators_pattern, operators);
+
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		std::cout << numbers[i] << " ";
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < operators.size(); i++)
+	{
+		std::cout << operators[i] << " ";
+	}
 
 	return 0;
 }
 
-void group(std::string input_string, std::regex pattern) {
-	// eventually it should group sequences described by pattern by searching input_string and save it to the list (which also should be
-	// given as a parameter)
+template<class T>
+void group(std::string input_string, std::regex pattern, std::vector<T>& container) {
+	// groups sequences described by given pattern by searching input_string and save it to the given vector
 
-	std::smatch number;
+	// to hold the matching sequence
+	std::smatch match; 
+	std::string match_str;
 
+	// to iterate through matching sequences
 	std::sregex_iterator words_begin =
 		std::sregex_iterator(input_string.begin(), input_string.end(), pattern);
 	std::sregex_iterator words_end = std::sregex_iterator();
 
 	for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
-		number = *i;
-		std::string match_str = number.str();
-		std::cout << match_str << '\n';
-	}
+		match = *i;
+		match_str = match.str();
 
-	// TODO: Use arraylist (is indexible :P) / list (isn't, but I guess it's more efficitent) / vector?
+		// if we're processing numbers, then cast the string to int
+		if (std::is_same<T, int>::value) {
+			int number;	
+			number = stoi(match_str);
+			container.push_back(number);
+		}
+
+		//if it's supposed to be a char, then cast it to char
+		if (std::is_same<T, char>::value) {
+			container.push_back(match_str[0]);
+		}
+	}
 
 }
