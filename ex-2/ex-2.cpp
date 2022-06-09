72dcbdd922c7b514cd6990541a0e55a5f902a3b3
MSlezak2 <michalsleezak@gmail.com>
2022-06-09 14:14:33 +0200

    Add performing 4 operations
    The equation containing of addition, subtraction, multiplication and / or
    division can now be solved with respect to operators' precedence.

--- ex-2/ex-2.cpp
+++ ex-2/ex-2.cpp
@@ -6,15 +6,19 @@
 
 template<class T>
 void group(std::string input_string, std::regex pattern, std::vector<T>& container);
+void addition_and_subtraction(std::vector<int>& numbers, std::vector<char>& operators);
+void multiplication_and_division(std::vector<int>& numbers, std::vector<char>& operators);
 
 int main() {
 
-	std::string users_input = "45 + 9 * 9 - 1 00 /5";
+	std::string users_input = "3+3*3";
 
 	// TODO: Validate user input (with regular expressions?, to make sure that there's for instance no such weird sequence like here: "45+-5")
 
-	std::regex numbers_pattern("[0-9]+");
-	std::regex operators_pattern("[+-/*]");
+	const std::regex numbers_pattern("[0-9]+");
+	const std::regex operators_pattern("[+-/*]");
+
+	// group numbers and operators into separate vectors for further processing
 
 	std::vector<int> numbers;
 	std::vector<char> operators;
@@ -22,16 +26,10 @@ int main() {
 	group(users_input, numbers_pattern, numbers);
 	group(users_input, operators_pattern, operators);
 
-	// display extracted groups:
-	for (int i = 0; i < numbers.size(); i++)
-	{
-		std::cout << numbers[i] << " ";
-	}
-	std::cout << std::endl;
-	for (int i = 0; i < operators.size(); i++)
-	{
-		std::cout << operators[i] << " ";
-	}
+	multiplication_and_division(numbers, operators);
+	addition_and_subtraction(numbers, operators);
+
+	std::cout << users_input << " = " << numbers[0];
 
 	return 0;
 }
@@ -66,4 +64,71 @@ void group(std::string input_string, std::regex pattern, std::vector<T>& contain
 		}
 	}
 
+}
+
+void addition_and_subtraction(std::vector<int>& numbers, std::vector<char>& operators) {
+	// TODO: There have to be certain relation between size of those containers (no_numbers = no_operators+1)
+	// otherwise return an error message
+
+	int i = 0;
+	std::vector<int>::iterator numbers_iterator;  // we need those iterators to use vector.erase() function
+	std::vector<char>::iterator operators_iterator;
+
+	while (i < operators.size()) {
+
+		// if it's the operation that we mean to perform, then take i-th and i+1-th numbers, do calculations,
+		// save result in i-th element, and delete i+1-th, then delete the current operator (don't increment i though...)
+		if (operators[i] == '+') { // addition
+			numbers[i] = numbers[i] + numbers[i + 1];
+			numbers_iterator = numbers.begin() + i + 1;
+			numbers.erase(numbers_iterator);
+			operators_iterator = operators.begin() + i;
+			operators.erase(operators_iterator);
+		}
+		else if (operators[i] == '/') { // subtraction
+			numbers[i] = numbers[i] - numbers[i + 1];
+			numbers_iterator = numbers.begin() + i + 1;
+			numbers.erase(numbers_iterator);
+			operators_iterator = operators.begin() + i;
+			operators.erase(operators_iterator);
+		}
+		// if it's not, increment i
+		else { // skip the rest
+			i++;
+		}
+	}
+}
+
+void multiplication_and_division(std::vector<int>& numbers, std::vector<char>& operators) {
+	// TODO: There have to be certain relation between size of those containers (no_numbers = no_operators+1)
+	// otherwise return an error message
+
+	int i = 0;
+	std::vector<int>::iterator numbers_iterator;  // we need those iterators to use vector.erase() function
+	std::vector<char>::iterator operators_iterator;
+
+	while (i < operators.size()) {
+
+		// if it's the operation that we mean to perform, then take i-th and i+1-th numbers, do calculations,
+		// save result in i-th element, and delete i+1-th, then delete the current operator (don't increment i though...)
+		if (operators[i] == '*') { // multiplication
+			numbers[i] = numbers[i] * numbers[i + 1];
+			numbers_iterator = numbers.begin() + i + 1;
+			numbers.erase(numbers_iterator);
+			operators_iterator = operators.begin() + i;
+			operators.erase(operators_iterator);
+		} 
+		else if (operators[i] == '/') { // division
+			numbers[i] = numbers[i] / numbers[i + 1];
+			numbers_iterator = numbers.begin() + i + 1;
+			numbers.erase(numbers_iterator);
+			operators_iterator = operators.begin() + i;
+			operators.erase(operators_iterator);
+		}
+		// if it's not, increment i
+		else { // skip the rest
+			i++;
+		}
+		std::cout << "test";
+	}
 }
