034df8ba85ae079f13039308116a85269f942f1e
MSlezak2 <michalsleezak@gmail.com>
2022-06-12 19:29:04 +0200

    Add decryption

--- ex-3/ex-3.cpp
+++ ex-3/ex-3.cpp
@@ -1,10 +1,13 @@
 #include <string>
 #include <iostream>
+#include <cmath>
 
-using std::string; using std::cout;
+using std::string; using std::cout; using std::endl; using std::min;
 
 string encrypt(string plain_text, string keyword);
 char encrypt_letter(char letter, char key);
+string decrypt(string encrypted_text, string keyword);
+char decrypt_letter(char letter, char key);
 string adjust_keyword(string plain_text, string keyword);
 int which_place_in_the_alphabet(char letter);
 
@@ -17,8 +20,12 @@ int main() {
 
 	encrypted_text = encrypt(plain_text, keyword);
 
-	cout << encrypted_text;
-	//decrypted_text = decrypt(encrypted_text, keyword);
+	decrypted_text = decrypt(encrypted_text, keyword);
+
+	cout << "plaint text: " << plain_text << endl;
+	cout << "keyword: " << keyword << endl;
+	cout << "encrypted text:" << encrypted_text << endl;
+	cout << "decrypted text:" << decrypted_text << endl;
 
 }
 
@@ -45,9 +52,47 @@ char encrypt_letter(char letter, char key) {
 
 	char encrypted_letter;
 	
-	encrypted_letter = 'A' + ((which_place_in_the_alphabet(letter) + which_place_in_the_alphabet(key)) % 26);
+	encrypted_letter = 'A' + ((which_place_in_the_alphabet(letter) + which_place_in_the_alphabet(key)) % 26); // modulo is because we don't want to go beyond Z-letter ;)
 	
 	return encrypted_letter;
+
+}
+
+string decrypt(string encrypted_text, string keyword) {
+
+	string decrypted_text;
+	string adjusted_keyword;
+
+	// prepare keyword (extend / shrink it to be as long as the plain text)
+	adjusted_keyword = adjust_keyword(encrypted_text, keyword);
+
+	char temp;
+	for (int i = 0; i < encrypted_text.length(); i++)
+	{
+		temp = decrypt_letter(encrypted_text.at(i), adjusted_keyword.at(i));
+		decrypted_text.append(1, temp);
+	}
+
+	return decrypted_text;
+
+}
+
+char decrypt_letter(char letter, char key) {
+
+	char decrypted_letter;
+
+	if (which_place_in_the_alphabet(key) <= which_place_in_the_alphabet(letter)) {
+
+		decrypted_letter = 'A' + (abs(which_place_in_the_alphabet(letter) - which_place_in_the_alphabet(key)) % 26); // modulo is because we don't want to go beyond Z-letter ;)
+
+	}
+	else {
+
+		decrypted_letter = 'A' + (26 - which_place_in_the_alphabet(key) + which_place_in_the_alphabet(letter));
+
+	}
+
+	return decrypted_letter;
 
 }
 
